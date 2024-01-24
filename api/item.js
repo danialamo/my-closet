79827b971f1a27981f33314dbece3f08e9970089
Dani Alamo <dca5244@psu.edu>
2024-01-23 22:15:52 -0500

    .

--- api/item.js
+++ api/item.js
@@ -1,14 +1,1 @@
-export default async function handler(req, res) {
-    const item = [{
-        "header": "T-Shirt",
-        "subheading": "A card with optional accent stylings.",
-        "content": "This card is highly customizable to contain any content you'd like",
-    }];
-    
-    res.setHeader('Cache-Control', 'max-age=0, s-maxage=1800');
-    res.setHeader("Access-Control-Allow-Credentials", "true");
-    res.setHeader("Access-Control-Allow-Origin", "*");
-    res.setHeader("Access-Control-Allow-Methods", "GET,OPTIONS,PATCH,DELETE,POST,PUT");
-    res.setHeader("Access-Control-Allow-Headers", "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version");
     res.json(item);
-  }
--- src/my-closet.js
+++ src/my-closet.js
@@ -1,54 +1,52 @@
 import { LitElement, html, css } from 'lit';
-import "@lrnwebcomponents/accent-card/accent-card.js";
+import "./item-card.js";
 
 
 class MyCloset extends LitElement {
-  static properties = {
-    header: { type: String },
-    subheading: { type: String },
-    content: { type: String },
-    image: {type: String}
+  static get tag(){
+    return 'my-closet';
   }
-
-  static styles = css`
-    :host {
-     
-      display: flex;
-      flex-direction: column;
-      align-items: center;
-      justify-content: flex-start;
-      font-size: calc(10px + 2vmin);
-      color: #1a2b42;
-      max-width: 700px;
-      margin: 0 auto;
-      text-align: center;
-      background-color: var(--my-closet-background-color);
+  static get properties(){
+    return{
+        items: {type: Array}
     }
-
-    main {
-      flex-grow: 1;
-    }
-  `;
-
-  constructor() {
-    super();
-    this.header = "Accent Card";
-    this.subheading = "A card with optional accent stylings.";
-    this.content = "This card is highly customizable to contain any content you'd like"; 
-    this.image = "https://cdn.shopify.com/s/files/1/0259/5448/4284/products/SKIMS-LOUNGEWEAR-AP-TNK-0282-ONX-FL_1456x_jpeg.jpg?v=1675404669&width=1200";
-  }
-
-  render() {
-    return html`
-      <main>
-      <accent-card image-src=${this.image} accent-color="red" horizontal accent-heading>
-       <div slot="heading">${this.header}</div>
-       <div slot="subheading">${this.subheading}</div>
-      <div slot="content"><p>${this.content}</p></div>
-      </accent-card>   
-      </main>
-    `;
-  }
+}
+constructor(){
+  super();
+  this.items = [{
+    "header": "T-Shirt",
+    "subheading": "A card with optional accent stylings.",
+    "content": "This card is highly customizable to contain any content you'd like",
+  }]; 
 }
 
+static get styles(){
+  return css `
+    :host{s
+      display: block;
+    }
+    .wrapper{
+      border: 2px solid black; 
+      display flex; 
+    }
+    .item{
+      display: inline-flex; 
+    }
+  `;
+}
+
+
+render(){
+  return html`
+  <div class= "wrapper">
+  ${this.items.map(item => html`
+  <div class="item">
+  <my-closet header="${item.header}" subheading="${item.subheading}" content="${item.content}" image="${item.image}"></my-closet>
+  </div>
+  `)}
+</div>
+  `
+}
+
+}
 customElements.define('my-closet', MyCloset);
